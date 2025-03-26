80c7ec5adb31d577e656c50cf73f7944931534c8
Kavya Mehta <kavyamehta044@gmail.com>
2025-03-25 19:01:58 -0700

    Working on Theme Toggle

--- script.js
+++ script.js
@@ -34,6 +34,29 @@ function showOrHide(){
   console.log(themePopupVisible)
   
 }
+ function applyTheme(theme){
+ document.body.className=theme
+ localStorage.setItem('selectedTheme',theme)//stores the selected theme
+
+ }
+ const themeCheckBoxes= document.querySelectorAll('input[name="theme"]')
+ function handleThemeSelection(selectedCheckBox){
+ for(let checkbox of themeCheckBoxes){
+  checkbox.checked=false;
+  selectedCheckBox.checked=true;
+  applyTheme(selectedCheckBox.value);
+ }
+
+ } 
+
+themeCheckBoxes.forEach(checkbox=>{
+  checkbox.addEventListener("change",handleThemeSelection)
+});
+document.addEventListener("DOMContentLoaded",function(){
+  const saveTheme=localStorage.getItem('selectedTheme') ||'Ocean';// grabbing selecting theme to reuse after page reloads
+  applyTheme(saveTheme);
+  // find the checkbox that matches the selected theme and check it
+})
 
 
 
